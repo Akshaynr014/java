2f4b0303a3eb53195c02c1dc46577b1e2078aaa0
Akshay N R <akshaynr014@gmail.com>
2025-07-01 15:39:13 +0530

    initial commit

---
+++ day7/IteratorExample.java
@@ -0,0 +1,24 @@
+package com.dyashincollection.cursor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorExample {
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<>();
+		al.add("akshay");
+		al.add("virat Lohli");
+		al.add("kl Rahul");
+		al.add("RCB");
+		Iterator<String> it = al.iterator();
+		while (it.hasNext()) {
+			String name = it.next();
+			if (name.equals("RCB")) {
+				it.remove();
+			} else {
+				System.out.println(name);
+			}
+
+		}
+	}
+}
